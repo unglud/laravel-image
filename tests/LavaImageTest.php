87b84469596479f380b2256746c0ed7c5964f0e6
unglued <alex@apus.ag>
2015-05-03 00:06:48 +0300

    add phpunit

---
+++ tests/LavaImageTest.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by Alexander 'unglued' Matrosov.
+ * Company: Apus Agency
+ * Site: http://www.apus.ag
+ * E-mail: alex@apus.ag
+ * Date: 03/05/15
+ * Copyright (c) 2006-2015 Apus Agency
+ */
+class LavaImageTest extends PHPUnit_Framework_TestCase {
+    function test_is_working(){
+        $this->assertTrue(true);
+    }
+}
